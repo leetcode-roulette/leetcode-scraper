bab1216d5014270123895caa26fd2e0a085ad06a
jared-ivory <26067982+Jared-Ivory@users.noreply.github.com>
2022-09-28 02:12:11 -0400

    Renamed PageEvents to PageEventsMap Added PageEvents enum

--- src/packages/LeetCodeBrowser/EventListeners.ts
+++ src/packages/LeetCodeBrowser/EventListeners.ts
@@ -1,13 +1,13 @@
 import { Browser, Page, PageEventObject } from "puppeteer";
-import { BrowserEvents, PageEvents } from "./events";
+import { BrowserEvents, PageEventsMap } from "./events";
 import EventEmitter from "eventemitter3";
 
 export function bindPageEventListeners(page: Page, context: EventEmitter) {
-	const pageEvents: (keyof PageEventObject)[] = Array.from(PageEvents.values());
+	const pageEvents: (keyof PageEventObject)[] = Array.from(PageEventsMap.values());
 	pageEvents.map((event) => {
 		page.on(event, (pageEvent) => {
 			const eventName = `page_${event}`;
-			context.emit(eventName, pageEvent);
+			context.emit(eventName, pageEvent, page);
 		});
 	});
 }
--- src/packages/LeetCodeBrowser/events/PageEvents.ts
+++ src/packages/LeetCodeBrowser/events/PageEvents.ts
@@ -1,6 +1,6 @@
 import { PageEventObject, PageEmittedEvents } from "puppeteer";
 
-const PageEvents = new Map<string, keyof PageEventObject>([
+const PageEventsMap = new Map<string, keyof PageEventObject>([
 	["page_close", PageEmittedEvents.Close],
 	["page_console", PageEmittedEvents.Console],
 	["page_dialog", PageEmittedEvents.Dialog],
@@ -22,4 +22,26 @@ const PageEvents = new Map<string, keyof PageEventObject>([
 	["page_domcontentloaded", PageEmittedEvents.DOMContentLoaded],
 ]);
 
-export default PageEvents;
+enum PageEvents {
+	PAGE_CLOSE = "page_close",
+	PAGE_CONSOLE = "page_console",
+	PAGE_DIALOG = "page_dialog",
+	PAGE_ERROR = "page_error",
+	PAGE_FRAME_ATTACHED = "page_frameattached",
+	PAGE_FRAME_DETACHED = "page_framedetached",
+	PAGE_FRAME_NAVIGATED = "page_framenavigated",
+	PAGE_LOAD = "page_load",
+	PAGE_METRICS = "page_metrics",
+	PAGE_PAGE_ERROR = "page_pageerror",
+	PAGE_POPUP = "page_popup",
+	PAGE_REQUEST = "page_request",
+	PAGE_REQUEST_FAILED = "page_requestfailed",
+	PAGE_REQUEST_FINISHED = "page_requestfinished",
+	PAGE_REQUEST_SERVED_FROM_CACHE = "page_requestservedfromcache",
+	PAGE_RESPONSE = "page_response",
+	PAGE_WORKER_CREATED = "page_workercreated",
+	PAGE_WORKER_DESTROYED = "page_workerdestroyed",
+	PAGE_DOM_CONTENT_LOADED = "page_domcontentloaded",
+}
+
+export { PageEvents, PageEventsMap };
