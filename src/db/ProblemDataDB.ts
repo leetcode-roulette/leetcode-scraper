da7319d75ee008a85f80a598bc7fa01c4bf21b62
jared-ivory <26067982+Jared-Ivory@users.noreply.github.com>
2022-09-29 14:18:04 -0400

    Added ProblemDataDB

---
+++ src/db/ProblemDataDB.ts
@@ -0,0 +1,26 @@
+import { IProblemData, ProblemData } from "../models/problemdata";
+import { iStats, LeetCodeQuestionData, QuestionDifficulty } from "../packages/LeetCodeBrowser/interfaces/LeetCodeQuestionData";
+
+export class ProblemDataDB {
+	public static async insert(questionData: LeetCodeQuestionData): Promise<void> {
+		let stats: iStats = JSON.parse(questionData.stats);
+		let data: IProblemData = await ProblemData.create({
+			problemID: questionData.questionId,
+			problemFrontendID: questionData.questionFrontendId,
+			title: questionData.title,
+			titleSlug: questionData.titleSlug,
+			content: questionData.content,
+			isPremium: questionData.isPaidOnly,
+			difficulty: QuestionDifficulty[questionData.difficulty],
+			likes: questionData.likes,
+			dislikes: questionData.dislikes,
+			similarQuestions: questionData.similarQuestions,
+			tags: questionData.topicTags,
+			stats: { accepted: stats.totalAcceptedRaw, submissions: stats.totalSubmissionRaw, acRate: stats.acRate },
+			hints: questionData.hints,
+			solution: questionData.solution,
+		});
+
+		await data.save();
+	}
+}
