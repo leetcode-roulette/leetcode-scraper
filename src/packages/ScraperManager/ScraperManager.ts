b1f63dbf154748decb3e75d8530a46e2a2f52094
jared-ivory <26067982+Jared-Ivory@users.noreply.github.com>
2022-10-07 23:18:18 -0400

    completed scrapermanager.ts

--- src/packages/ScraperManager/ScraperManager.ts
+++ src/packages/ScraperManager/ScraperManager.ts
@@ -1,10 +1,12 @@
 import { EventEmitter } from "eventemitter3";
 import { getTitleSlugs } from "../../db/Problem";
 import LeetcodeBrowser from "../LeetCodeBrowser/LeetcodeBrowser";
+import { ProblemDataDB } from "../../db/ProblemDataDB";
+import { LeetcodeQuestionData } from "../LeetCodeBrowser/interfaces/LeetcodeQuestionData";
 
 class ScraperManager extends EventEmitter {
 	private static LeetcodeBrowser: LeetcodeBrowser | null = null;
-	private static titleSlugs: string[] = [];
+	private static titleSlugs: string[] | undefined = undefined;
 
 	private async getLeetcodeBrowser() {
 		if (!ScraperManager.LeetcodeBrowser) {
@@ -23,10 +25,24 @@ class ScraperManager extends EventEmitter {
 	private async scrape(titleSlug: string) {
 		const LeetcodeBrowser = await this.getLeetcodeBrowser();
 		await LeetcodeBrowser.goTo(titleSlug);
+
+		const responses = LeetcodeBrowser.responses;
+
+		const questionData = responses.get("questionData");
+		if (questionData) {
+			await this.insert(questionData.data.question);
+		}
+	}
+
+	private async insert(questionData: LeetcodeQuestionData) {
+		await ProblemDataDB.insert(questionData);
 	}
 	public async start() {
-		const titleSlugs = await getTitleSlugs();
-		this.scrape(titleSlugs[Math.floor(Math.random() * titleSlugs.length - 1)]);
+		let titleSlugs = await this.getTitleSlugs();
+		for (let i = 0; i < titleSlugs.length; i++) {
+			await this.scrape(titleSlugs[i]);
+			console.log(`Scraped: ${titleSlugs[i]} |\t\t${i} of ${titleSlugs.length}`);
+		}
 	}
 }
 
