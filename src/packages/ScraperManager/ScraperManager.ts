92887bcde5e6224f4b9ed52f1a5be4a52013bb6f
jared-ivory <26067982+Jared-Ivory@users.noreply.github.com>
2022-10-07 23:36:29 -0400

    Changed file names added cli-progress

--- src/packages/ScraperManager/ScraperManager.ts
+++ src/packages/ScraperManager/ScraperManager.ts
@@ -10,7 +10,7 @@ class ScraperManager extends EventEmitter {
 
 	private async getLeetcodeBrowser() {
 		if (!ScraperManager.LeetcodeBrowser) {
-			ScraperManager.LeetcodeBrowser = await LeetcodeBrowser.createInstance({ headless: false });
+			ScraperManager.LeetcodeBrowser = await LeetcodeBrowser.createInstance();
 		}
 		return ScraperManager.LeetcodeBrowser;
 	}
@@ -37,10 +37,21 @@ class ScraperManager extends EventEmitter {
 	private async insert(questionData: LeetcodeQuestionData) {
 		await ProblemDataDB.insert(questionData);
 	}
-	public async start() {
+
+	/**
+	 * // TODO: comment getScriptVersion
+	 * Sets node timeout
+	 * @param delay: ms
+	 * @returns Promise<void>
+	 */
+	async timeout(delay: number): Promise<void> {
+		return new Promise((res) => setTimeout(res, delay));
+	}
+	public async start(delay: number = 1000) {
 		let titleSlugs = await this.getTitleSlugs();
 		for (let i = 0; i < titleSlugs.length; i++) {
 			await this.scrape(titleSlugs[i]);
+			await this.timeout(delay);
 			console.log(`Scraped: ${titleSlugs[i]} |\t\t${i} of ${titleSlugs.length}`);
 		}
 	}
